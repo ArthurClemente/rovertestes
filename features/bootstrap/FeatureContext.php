4371771134ae7dd7f6d9659696fd6481eb16fe95
ArthurClemente <arthurclementte@hotmail.com>
2022-06-15 13:30:51 -0300

    Criado o teste de alterar a direção do plato de N para O.

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -4,25 +4,44 @@ use Behat\Behat\Context\Context;
 use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use features\bootstrap\Plato;
+use features\bootstrap\Rover;
+use features\bootstrap\RoverSquad;
+use PHPUnit\Framework\Assert;
 
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext implements Context
 {
+    private Plato $plato;
+    private Rover $rover;
+
+    // Testes de mover Rover
     /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
+     * @Given que a direcao do plato é :direcao
      */
-    public function __construct()
+    public function direcaoDoPlatoE($direcao){
+        $this->plato = new Plato("5 5");
+        $this->rover = new Rover(1, 3, $direcao, $this->plato);
+    }
+    
+    /**
+     * @When o parametro da funcao mover for :movimento
+     */
+    public function parametroDaFuncaoMoverFor($movimento)
     {
+        $this->rover->mover($movimento);
     }
 
-    // Testes de adicionar Rover
     /**
-     * @Given que a :arg1 do plato é :arg2
+     * @Then a direcao do rover deve ser igual :direcao
      */
+    public function direcaoDoRoverDeveSerIgualA($direcao)
+    {
+        PHPUnit\Framework\Assert::assertSame(
+            $direcao,
+            $this->rover->getDirecaoRover()
+        );
+    }
 }
